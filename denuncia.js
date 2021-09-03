a7d8fb250f1c3585ea0b4c391da69c17c14bfccf
kiniun1 <levi.marcos17@hotmail.com>
2021-09-03 20:42:42 -0300

    certifica que RevRouter retorna 400 se nenhuma latitude or longitude for fornecida

--- denuncia.js
+++ denuncia.js
@@ -1,15 +1,31 @@
-const express = require('express')
-const router = express.Router()
+const express = require('restify')
+const router = restify.router()
 const mysql = require('mysql')
 const DenunciaModel = mysql.model('Denuncia')
 
 module.exports = () => {
-    router.post('/v1/denuncias', new CallGeocodingApiRouter().route)
+    const router = new CallGeocodingApiRouter()
+    router.post('/v1/denuncias', RestifyRouterAdapter.adapt(router))
+}
+
+class RestifyRouterAdapter {
+    static adapt (router) {
+        return async (req, res) => {
+            const httpRequest = {
+                body: req.body
+            }
+            router.route(httpRequest)
+            res.status(httpResponse.statusCode).json(httpResponse.body)
+        }
+    }
 }
 
 class CallGeocodingApiRouter {
-    async route (req, res) {
-        const { latitude, longitude, nome, cpf, titulo, descricao} = req.body
-        
+    async route (httpRequest) {
+        const { latitude, longitude, nome, cpf, titulo, descricao} = httpRequest.body
+        return {
+            statusCode: 200,
+            body: user
+        }
     }
 }
---
+++ src/presentation/routers/revgeocoding-router.spec.js
@@ -0,0 +1,42 @@
+class RevGeocodingRouter {
+    route (httpRequest) {
+        if (!httpRequest.body.latitude) {
+            return {
+                statusCode: 400
+            }
+        }
+
+        if (!httpRequest.body.longitude) {
+            return{
+                statusCode: 400
+            }
+        }
+    }
+}
+
+
+describe('RevGeocoding Router', () => {
+    test('Deve retornar 400 se nenhuma latitude for fornecida.', () => {
+        const sut = new RevGeocodingRouter()
+        const httpRequest = {
+            body: {
+                longitude: 'any-longitude'
+            }
+        }
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+})
+
+describe('RevGeocoding Router', () => {
+    test('Deve retornar 400 se nenhuma latitude for fornecida.', () => {
+        const sut = new RevGeocodingRouter()
+        const httpRequest = {
+            body: {
+                latitude: 'any-latitude'
+            }
+        }
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+})
