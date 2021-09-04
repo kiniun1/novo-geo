1f9f3a98c464be1bc1facc9eb52f72a19f548eb2
kiniun1 <levi.marcos17@hotmail.com>
2021-09-03 21:48:18 -0300

    adicionados testes de parametros da denuncia para checar se estao vazios

---
+++ src/presentation/routers/denuncia-router.spec.js
@@ -0,0 +1,102 @@
+class DenunciaRouter {
+    route (httpRequest) {
+        const { latitude, longitude, nome, cpf, titulo, descricao } = httpRequest.body
+        if (!latitude || !longitude || !nome || !cpf || !titulo || !descricao) {
+            return {
+                statusCode: 400
+            }
+        }
+    }
+}
+
+describe('Denuncia Router', () => {
+    test('Deve retornar 400 se nenhuma latitude for fornecida.', () => {
+        const sut = new DenunciaRouter()
+        const httpRequest = {
+            body: {
+                longitude: 'any-longitude',
+                nome: 'any-nome',
+                cpf: 'any-cpf',
+                titulo: 'any-titulo',
+                descricao: 'any-descricao'
+            }
+        }
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+
+    test('Deve retornar 400 se nenhuma longitude for fornecida.', () => {
+        const sut = new DenunciaRouter()
+        const httpRequest = {
+            body: {
+                latitude: 'any-latitude',
+                nome: 'any-nome',
+                cpf: 'any-cpf',
+                titulo: 'any-titulo',
+                descricao: 'any-descricao'
+            }
+        }
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+
+    test('Deve retornar 400 se nenhum nome for fornecido.', () => {
+        const sut = new DenunciaRouter()
+        const httpRequest = {
+            body: {
+                longitude: 'any-longitude',
+                latitude: 'any-latitude',
+                cpf: 'any-cpf',
+                titulo: 'any-titulo',
+                descricao: 'any-descricao'
+            }
+        }
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+
+    test('Deve retornar 400 se nenhum cpf for fornecido.', () => {
+        const sut = new DenunciaRouter()
+        const httpRequest = {
+            body: {
+                longitude: 'any-longitude',
+                latitude: 'any-latitude',
+                nome: 'any-nome',
+                titulo: 'any-titulo',
+                descricao: 'any-descricao'
+            }
+        }
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+
+    test('Deve retornar 400 se nenhum titulo for fornecido.', () => {
+        const sut = new DenunciaRouter()
+        const httpRequest = {
+            body: {
+                longitude: 'any-longitude',
+                latitude: 'any-latitude',
+                nome: 'any-nome',
+                cpf: 'any-cpf',
+                descricao: 'any-descricao'
+            }
+        }
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+
+    test('Deve retornar 400 se nenhuma descrição for fornecida.', () => {
+        const sut = new DenunciaRouter()
+        const httpRequest = {
+            body: {
+                longitude: 'any-longitude',
+                latitude: 'any-latitude',
+                nome: 'any-nome',
+                cpf: 'any-cpf',
+                titulo: 'any-titulo'
+            }
+        }
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+})
