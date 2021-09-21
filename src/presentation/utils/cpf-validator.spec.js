43e886e066ef8382cd8088ba4c76e26797940f8c
kiniun1 <levi.marcos17@hotmail.com>
2021-09-21 16:30:08 -0300

    Adicionado as dependencias o pacote de validador de cpf, e adicionado os testes para retornar true se o método de validar o cpf retornar true, e false se o método retornar false

--- src/presentation/utils/cpf-validator.spec.js
+++ src/presentation/utils/cpf-validator.spec.js
@@ -1,13 +1,21 @@
+const { cpf } = require('cpf-cnpj-validator')
+
 class CpfValidator {
-  isValid(cpf) {
-    return true
+  isValid(cpfValor) {
+    return cpf.isValid(cpfValor)
   }
 }
 
 describe('Cpf Validator', () => {
   test('Deve retornar true se o validador retornar true', () => {
     const sut = new CpfValidator()
-    const isCpfValid = sut.isValid('12345678909')
+    const isCpfValid = sut.isValid('543.077.300-03')
     expect(isCpfValid).toBe(true)
+  })
+
+  test('Deve retornar false se o validador retornar false', () => {
+    const sut = new CpfValidator()
+    const isCpfValid = sut.isValid('00000000000')
+    expect(isCpfValid).toBe(false)
   })
 })
