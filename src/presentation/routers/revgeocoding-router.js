4e1dfdaabd23a1cc39e292d98e4c41a0ffaa178b
kiniun1 <levi.marcos17@hotmail.com>
2021-09-22 23:35:00 -0300

    adicionado arquivos do router da api externa revgeocoding, e o arquivo de teste.spec da rota com alguns testes ja adicionados

--- src/presentation/routers/revgeocoding-router.js
+++ src/presentation/routers/revgeocoding-router.js
@@ -4,17 +4,17 @@ const httpResponse = require('../helpers/http-response')
 module.exports = class RevGeocodingRouter {
   async route(httpRequest) {
     try {
-      const { latitude, longitude } = httpRequest.body
-      if (!latitude) {
+      const { lat, lng } = httpRequest.body.location.latLng
+      if (!lat) {
         return httpResponse.badRequest(new MissingParamError('latitude'))
       }
-      if (!longitude) {
+      if (!lng) {
         return httpResponse.badRequest(new MissingParamError('longitude'))
       }
-      if (isNaN(latitude)) {
+      if (isNaN(lat)) {
         return httpResponse.badRequest(new InvalidParamError('latitude'))
       }
-      if (isNaN(longitude)) {
+      if (isNaN(lng)) {
         return httpResponse.badRequest(new InvalidParamError('longitude'))
       }
       return httpResponse.ok()
