238543587bafa846a21d17f2ac3ab5a3d6812b32
kiniun1 <levi.marcos17@hotmail.com>
2021-09-23 12:01:32 -0300

    Adicionado testes de erro ao cpfValidator

--- src/utils/cpf-validator.js
+++ src/utils/cpf-validator.js
@@ -1,7 +1,14 @@
 const cpf = require('cpf-cnpj-validator')
+const { MissingParamError, InvalidParamError } = require('./errors')
 
 module.exports = class CpfValidator {
   isCpfValid(cpfValor) {
+    if (!cpfValor) {
+      throw new MissingParamError('cpf')
+    }
+    if (isNaN(cpfValor)) {
+      throw new InvalidParamError('cpf')
+    }
     return cpf.isValid(cpfValor)
   }
 }
--- src/utils/cpf-validator.spec.js
+++ src/utils/cpf-validator.spec.js
@@ -1,5 +1,6 @@
 const CpfValidator = require('./cpf-validator')
 const cpf = require('cpf-cnpj-validator')
+const { MissingParamError, InvalidParamError } = require('./errors')
 
 const makeSut = () => {
   return new CpfValidator()
@@ -23,5 +24,20 @@ describe('Cpf Validator', () => {
     const sut = makeSut()
     sut.isCpfValid('12345678901')
     expect(cpf.cpf).toBe('12345678901')
+  })
+
+  test('Deve fazer um throw se não for fornecido um cpf', () => {
+    const sut = makeSut()
+    invalid = null
+    expect(() => {
+      sut.isCpfValid(invalid)
+    }).toThrow(new MissingParamError('cpf'))
+  })
+
+  test('Deve fazer um throw se o cpf fornecido for não numérico', () => {
+    const sut = makeSut()
+    expect(() => {
+      sut.isCpfValid('any-non-number-cpf')
+    }).toThrow(new InvalidParamError('cpf'))
   })
 })
