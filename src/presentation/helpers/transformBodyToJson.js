63ba594c0992d7e88b1026ab1ea0a11ff5bec927
kiniun1 <levi.marcos17@hotmail.com>
2021-09-23 01:24:38 -0300

    Adicionado checagem de erros no método convert do transformBodyToJson.js

--- src/presentation/helpers/transformBodyToJson.js
+++ src/presentation/helpers/transformBodyToJson.js
@@ -1,5 +1,22 @@
+const { MissingParamError, InvalidParamError } = require('../../utils/errors')
+
 module.exports = class TransformData {
-  convert(data) {
+  async convert(data) {
+    if (!data) {
+      throw new MissingParamError('data')
+    }
+    if (!data.body.latitude) {
+      throw new MissingParamError('latitude')
+    }
+    if (!data.body.longitude) {
+      throw new MissingParamError('longitude')
+    }
+    if (isNaN(data.body.latitude)) {
+      throw new InvalidParamError('latitude')
+    }
+    if (isNaN(data.body.longitude)) {
+      throw new InvalidParamError('longitude')
+    }
     const { latitude, longitude } = data.body
     const latLng = {
       location: {
