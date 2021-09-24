5117318fc7c4593c2bdae5cde0c3b8cb4b9f7def
kiniun1 <levi.marcos17@hotmail.com>
2021-09-24 17:22:50 -0300

    adicionado testes para checar se a busca no banco pelas denuncias pelo cpf e titulo, e o arquivo js, com a classe e o método

--- src/infra/repositories/load-denuncia-by-cpf-repository.js
+++ src/infra/repositories/load-denuncia-by-cpf-repository.js
@@ -1,10 +1,32 @@
+const MongoHelper = require('../helpers/mongo-helper')
+const MissingParamError = require('../../utils/errors/missing-param-error')
+
 module.exports = class LoadDenunciaByCpfRepository {
   constructor(denunciaModel) {
     this.denunciaModel = denunciaModel
   }
 
-  async load(cpf) {
-    const denunciaDb = await this.denunciaModel.findOne({ cpf: cpf })
-    return denunciaDb
+  async load(cpf, titulo) {
+    if (!cpf) {
+      throw new MissingParamError('cpf')
+    }
+    if (!titulo) {
+      throw new MissingParamError('titulo')
+    }
+    const denunciaModel = await MongoHelper.pegandoColeções('denuncias')
+    const denuncia = await denunciaModel.findOne(
+      {
+        cpf,
+        titulo,
+      },
+      {
+        projection: {
+          cpf: 1,
+          titulo: 1,
+          descricao: 1,
+        },
+      }
+    )
+    return denuncia
   }
 }
---
+++ src/infra/repositories/load-denuncia-by-cpf-repository.spec.js
@@ -0,0 +1,63 @@
+const LoadDenunciaByCpfRepository = require('./load-denuncia-by-cpf-repository')
+const MongoHelper = require('../helpers/mongo-helper')
+const MissingParamError = require('../../utils/errors/missing-param-error')
+
+let denunciaModel
+
+const makeSut = () => {
+  return new LoadDenunciaByCpfRepository()
+}
+
+describe('Inserir Denuncias', () => {
+  beforeAll(async () => {
+    rota = 'mongodb://localhost:27017'
+    await MongoHelper.connect(rota)
+    denunciaModel = await MongoHelper.pegandoColeções('denuncias')
+  })
+
+  beforeEach(async () => {
+    await denunciaModel.deleteMany()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.fechando()
+  })
+  test('Deve retornar o cpf, nome, titulo e descrição da denuncia gerado pela busca por denuncias registradas naquele cpf e titulo', async () => {
+    const sut = makeSut()
+    const fakeDenuncia = {
+      cpf: '12345678909',
+      nome: 'any_name',
+      titulo: 'any-titulo',
+      descricao: 'any_descricao',
+      logradouro: 'any-logradouro',
+      bairro: 'any-bairro',
+      cidade: 'any-cidade',
+      estado: 'any-estado',
+      pais: 'any-pais',
+      cep: 'any-cep',
+      latitude: 'any-latitude',
+      longitude: 'any-longitude',
+    }
+    const retorno = await denunciaModel.insertOne(fakeDenuncia)
+    const denuncia = await sut.load('12345678909', 'any-titulo')
+    expect(denuncia).toEqual({
+      _id: retorno.insertedId,
+      cpf: fakeDenuncia.cpf,
+      titulo: fakeDenuncia.titulo,
+      descricao: fakeDenuncia.descricao,
+    })
+  })
+
+  test('Deve throwar se nenhum cpf for fornecido', async () => {
+    const sut = makeSut()
+    const promise = sut.load()
+    expect(promise).rejects.toThrow(new MissingParamError('cpf'))
+  })
+
+  test('Deve throwar se nenhum titulo for fornecido', async () => {
+    const sut = makeSut()
+    const cpf = '12345678909'
+    const promise = sut.load(cpf)
+    expect(promise).rejects.toThrow(new MissingParamError('titulo'))
+  })
+})
