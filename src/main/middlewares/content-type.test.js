e895dfa832cdc39801e7cb472dfc336d0b1e9de8
kiniun1 <levi.marcos17@hotmail.com>
2021-10-01 19:17:30 -0300

    resetar os modulos do jest no content-type middleware

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -1,11 +1,25 @@
 const request = require('supertest')
-const app = require('../config/app')
 
 describe('Content-Type Middleware', () => {
+  let app
+
+  beforeEach(() => {
+    jest.resetModules()
+    app = require('../config/app')
+  })
+
   test('Deve retornar content type JSON como padrão', async () => {
     app.get('/teste_content_type', (req, res) => {
       res.send('')
     })
     await request(app).get('/teste_content_type').expect('content-type', /json/)
+  })
+
+  test('Deve retornar content type xml quando forçado', async () => {
+    app.get('/teste_content_type', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app).get('/teste_content_type').expect('content-type', /xml/)
   })
 })
