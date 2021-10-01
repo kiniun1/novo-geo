a86e71c4b329e9a1434b685f7370e80c77a6bc75
kiniun1 <levi.marcos17@hotmail.com>
2021-10-01 18:01:42 -0300

    habilitado o CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -9,4 +9,14 @@ describe('App Setup', () => {
     const res = await request(app).get('/teste_x_powered_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
+
+  test('Deve habilitar o CORS', async () => {
+    app.get('/teste_cors', (req, res) => {
+      res.send('')
+    })
+    const res = await request(app).get('/teste_cors')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
+  })
 })
