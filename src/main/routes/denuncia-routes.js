13af9cf8370ff088f2bad0550403e62758f2b6a4
kiniun1 <levi.marcos17@hotmail.com>
2021-10-10 22:38:48 -0300

    adicionado o adapter de rotas do express

--- src/main/routes/denuncia-routes.js
+++ src/main/routes/denuncia-routes.js
@@ -1,6 +1,6 @@
 const denunciaRouter = require('../composer/denuncia-router-composer')
-const expressRouterAdapter = require('../adapters/express-router-adapter')
+const ExpressRouterAdapter = require('../adapters/express-router-adapter')
 
 module.exports = (router) => {
-  router.post('/v1/denuncia', denunciaRouter)
+  router.post('/v1/denuncia', ExpressRouterAdapter.adapt(denunciaRouter))
 }
