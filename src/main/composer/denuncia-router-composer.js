905d0cfffb589210664474f1a933c9bf1cd01fcd
kiniun1 <levi.marcos17@hotmail.com>
2021-10-14 18:13:00 -0300

    adicionado a criação da instancia do cache ao composer do denuncia-router

--- src/main/composer/denuncia-router-composer.js
+++ src/main/composer/denuncia-router-composer.js
@@ -2,17 +2,20 @@ const DenunciaRouter = require('../../presentation/routers/denuncia-router')
 const CpfValidator = require('../../utils/cpf-validator')
 const SaveDenuncia = require('../../infra/save-denuncia')
 const RevGeocoding = require('../../utils/rev-geocoding')
+const Cache = require('../../infra/cache')
 
 module.exports = class DenunciaRouterComposer {
   static compose() {
     const cpfValidator = new CpfValidator()
     const saveDenuncia = new SaveDenuncia()
     const revGeocoding = new RevGeocoding()
+    const cache = new Cache()
 
     return new DenunciaRouter({
       cpfValidator,
       saveDenuncia,
       revGeocoding,
+      cache,
     })
   }
 }
